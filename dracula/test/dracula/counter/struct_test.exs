1fed55071b723a1d592aa89155e2fca15958bde2
Rachel Fischoff <rfischoff@corvusinsurance.com>
2023-10-18 11:38:44 -0400

    TEST: CRC test to struct

--- dracula/test/dracula/counter/struct_test.exs
+++ dracula/test/dracula/counter/struct_test.exs
@@ -7,11 +7,20 @@ defmodule Dracula.Counter.StructTest do
     result =
       "7"
       |> Struct.new()
+      |> assert_count(7)
       |> Struct.inc()
+      |> assert_count(8)
       |> Struct.inc()
+      |> assert_count(9)
       |> Struct.dec()
+      |> assert_count(8)
       |> Struct.show()
 
     assert "ahoy m8ty" == result
+  end
+
+  def assert_count(acc, count) do
+    assert acc.count == count
+    acc
   end
 end
